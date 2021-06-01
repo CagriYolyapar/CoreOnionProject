1c8bb0b6eb405198d55d224762e2e808597959a8
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-06-01 13:54:06 +0300

    SessionExtension olusturuldu

---
+++ Project.COMMON/Tools/SessionExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.COMMON.Tools
+{
+    //Todo:BLL katmanına gecilecek
+    public static class SessionExtension
+    {
+        public static void SetObject(this ISession session,string key,object value)
+        {
+            string objectString = JsonConvert.SerializeObject(value);
+            session.SetString(key, objectString);
+        }
+
+        public static T GetObject<T>(this ISession session,string key) where T : class
+        {
+            string objectString = session.GetString(key) ;
+            if (string.IsNullOrEmpty(objectString)) return null;
+            T deserializedObject = JsonConvert.DeserializeObject<T>(objectString);
+            return deserializedObject;
+        }
+    }
+}
